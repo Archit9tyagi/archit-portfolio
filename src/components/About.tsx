01e8d74ffacb7a70d5b411e8cd48526f229987d7
Archit Tyagi <archittyagi462@gmail.com>
2026-03-10 11:08:56 +0530

    Initial commit for Archit's Portfolio

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I’m an aspiring Software Developer with a strong focus on Full-Stack Web Development. I enjoy building modern, scalable web applications and creating intuitive user experiences.
+          <br /><br />
+          Alongside web development, I have hands-on experience working on Machine Learning and Data Science projects using Python, where I explore data-driven approaches to solve real-world problems.
+          <br /><br />
+          I’m passionate about writing clean, efficient code, building impactful projects, and continuously learning new technologies to grow as a developer.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
